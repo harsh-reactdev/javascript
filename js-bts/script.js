b8abe40057cdfe065435a4180fe25fc801cfd8a0
harsh-reactdev <harsh.reactdev@gmail.com>
2024-03-14 12:16:47 +0530

    chore: completed section js behind the scenes

--- js-bts/script.js
+++ js-bts/script.js
@@ -81,4 +81,120 @@ let z = 3;
 
 
 // -----------------------------------------------------
-// The 'this' keybord
+// The 'this' keyword
+
+//global
+// console.log(this); //points to global window object
+
+//simple function
+function minus(a, b) {
+    // console.log(a - b);
+    // console.log(this); //undefined for a normal function
+}
+minus(50, 20);
+
+
+// arrow functions
+const minusArrow = (a, b) => {
+    // console.log(a - b);
+    // console.log(this);
+    //points to this value of parent scope, here window object
+    // arrow functions do not have any this keyword of its own. So it takes the this keyword of its immediate lexical scope
+};
+minusArrow(60, 39);
+
+
+//this in object method
+const harsh = {
+    name: 'harsh',
+    birthyear: 2000,
+    calcAge: function () {
+        this.age = 2024 - this.birthyear;
+        // console.log(this);
+    }
+};
+harsh.calcAge();//here it points to the object harsh
+
+const tum = {
+    name: 'tum',
+    birthyear: 1998,
+};
+tum.calcAge = harsh.calcAge; //this is called method borrowing. Simply copying the method of one object to another oject
+
+tum.calcAge(); //this points to the object with which we call the method. So here, this points to the object tum
+
+
+const fInMethod = {
+    a: 20,
+    b: 3,
+    calcDiff: function () {
+        this.diff = this.a - this.b;
+
+        // to preserve the value of this for normal function calls
+        const self = this;
+        const c = function () {
+            console.log(this);
+            //value of this is undefined as this for any normal function calls is set to undefined
+
+            // console.log(self); This is a pre-ES6 solution
+            // this will preserve the value of this by storing it in an extra variable called self here
+        };
+        c();
+
+        // this is a more modern solution to preserve the value of this. Simply use arrow functions, if ever there is a need for functions inside methods
+        const d = () => {
+            console.log(this);
+            //will have value of this of calcDiff function as it is an arrow function
+            // this is 
+        };
+        d();
+
+    }
+};
+
+// fInMethod.calcDiff();
+
+
+// ---------------------------------------------------------------
+// Primitive vs. reference types
+
+// primitive types
+let fullname = 'harshith m r';
+let oldName = fullname;
+fullname = 'harshith shetty';
+
+// console.log('old name: ', oldName);
+// console.log('new name: ', fullname);
+
+
+// reference type
+const me = {
+    fName: 'harshith',
+    lName: 'shetty'
+};
+
+const cousin = me;
+
+cousin.fName = 'chandan'; //changes in both me object and in cousin object as they are both pointing to same object in the heap
+// console.log('me2 : ', me2);
+// console.log('cousin2 : ', cousin2);
+
+
+// instead, inorder to actually copy objects and not just the reference
+const me2 = {
+    fName: 'harshith',
+    lName: 'shetty',
+    friends: ['Akhil', 'Jeevan', 'Mahesh']
+};
+
+const cousin2 = Object.assign({}, me2);//creates a shallow copy
+cousin2.fName = 'chandan';
+
+cousin2.friends.pop();
+cousin2.friends.pop();
+cousin2.friends.pop(); //changes in the me2 object too
+
+console.log('me2 : ', me2);
+console.log('cousin2 : ', cousin2);
+
+// inorder to deep clone a object, we use an external library like lodash for example which comes with a ton of helpful tools
