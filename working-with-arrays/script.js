fe5ac33c6789990a97cc6057ec253291c0036025
harsh-reactdev <harsh.reactdev@gmail.com>
2024-04-08 23:59:48 +0530

    chore: slice method

--- working-with-arrays/script.js
+++ working-with-arrays/script.js
@@ -82,3 +82,5 @@ let arr = ['a', 'b', 'c', 'd', 'e'];
 console.log(arr.slice(1)); //extracts everything from the 1st index to end of the array
 
 console.log(arr.slice(1, 3)); //extracts everything from 1st index to 2nd index and 3rd index wont be included
+
+console.log(arr.slice(0, -3)); //extracts ecer
