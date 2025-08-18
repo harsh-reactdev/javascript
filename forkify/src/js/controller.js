8f446cb8523837fa7de7676b51c18d13c3bda06c
harsh <harsh.reactdev@gmail.com>
2025-08-18 23:46:51 +0530

    chore: getting single recipe

--- forkify/src/js/controller.js
+++ forkify/src/js/controller.js
@@ -9,5 +9,34 @@ const timeout = function (s) {
 };
 
 // https://forkify-api.herokuapp.com/v2
-
 ///////////////////////////////////////
+
+
+// getting single recipe
+const showRecipe = async function () {
+  try {
+    const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886');
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message}`);
+
+    const { recipe: rec } = data.data;
+    let recipe = {
+      cookingTime: rec.cooking_time,
+      id: rec.id,
+      imageUrl: rec.image_url,
+      ingredients: rec.ingredients,
+      publisher: rec.publisher,
+      servings: rec.servings,
+      sourceUrl: rec.source_url,
+      title: rec.title,
+    };
+    console.log(recipe);
+
+    // return data;
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
+showRecipe();
